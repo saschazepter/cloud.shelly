9e76fff58c563fa6fa1e9515294a9e54fe5012fb
Tobias Feijten <tobias93@live.nl>
2025-05-20 13:57:25 +0200

    Add 1 Gen4 Zigbee driver

---
+++ drivers/shelly_zigbee_1_gen4/device.ts
@@ -0,0 +1,14 @@
+import {ZigBeeDevice} from 'homey-zigbeedriver';
+import {ZigBeeNode} from 'homey';
+import {ZCLNode} from 'zigbee-clusters';
+import initOnOffDevice from '@drenso/homey-zigbee-library/capabilities/onOff';
+
+module.exports = class Shelly1PMMiniGen4ZigbeeDevice extends ZigBeeDevice {
+
+  async onNodeInit(payload: { zclNode: ZCLNode; node: ZigBeeNode}): Promise<void> {
+    await super.onNodeInit(payload);
+
+    await initOnOffDevice(this, payload.zclNode);
+  }
+
+};
